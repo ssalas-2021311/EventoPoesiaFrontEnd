2b2895cf9a6b7c2bda62886b6f17dfbdde929132
ssalas-2021311 <ssalas-2021311@kinal.edu.gt>
2023-08-16 23:13:46 -0600

    first commit

--- src/formulario/components/UserForm.jsx
+++ src/formulario/components/UserForm.jsx
@@ -27,15 +27,49 @@ export const UserForm = () => {
     return (
 
         <>
-            <div className="contenedor">
+            <div className="container col-xxl-4 col-sm-12 formulario">
                 <div className="mb-3">
-                    <label for="formGroupExampleInput" className="form-label titulo">Carnet:</label>
-                    <input type="text" className="form-control texto" id="formGroupExampleInput" placeholder="Example input placeholder" />
+                    <label className="form-label titulo">Carnet:</label>
+                    <input type="text" className="form-control texto" id="formGroupExampleInput" required />
                 </div>
                 <div className="mb-3">
-                    <label for="formGroupExampleInput2" className="form-label titulo">Nombre completo:</label>
-                    <input type="text" className="form-control texto" id="formGroupExampleInput2" placeholder="Another input placeholder" />
+                    <label className="form-label titulo">Nombre completo:</label>
+                    <input type="text" className="form-control texto" id="formGroupExampleInput2" required />
                 </div>
+                <div className="mb-3">
+                    <label className="form-label titulo">Dirección:</label>
+                    <input type="text" className="form-control texto" id="formGroupExampleInput2" required />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label titulo">Género:</label>
+                    <select className="form-select" aria-label="Default select example" required>
+                        <option selected>Selecciona una opcion</option>
+                        <option value="1">Masculino</option>
+                        <option value="2">Femenino</option>
+                    </select>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label titulo">Teléfono:</label>
+                    <input type="number" className="form-control texto" id="formGroupExampleInput2" required />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label titulo">Fecha de nacimiento:</label>
+                    <input type="date" className="form-control texto" id="formGroupExampleInput2" required />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label titulo">Carrera:</label>
+                    <input type="text" className="form-control texto" id="formGroupExampleInput2" required />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label titulo">Género de poesía:</label>
+                    <select className="form-select" aria-label="Default select example" required>
+                        <option selected>Selecciona una opcion</option>
+                        <option value="1">Dramático</option>
+                        <option value="2">Épico</option>
+                        <option value="3">Lírica</option>
+                    </select>
+                </div>
+                <button class="btn btn-primary" type="submit">Registrarse</button>
             </div>
 
         </>
--- src/formulario/models/user.model.js
+++ src/formulario/models/user.model.js
@@ -1,11 +1,10 @@
 export const User = {
-    _id: "",
     carnet: "",
     nombre: "",
     direccion: "",
     genero: "",
     telefono: "",
     nacimiento: "",
-    ingresos_mensuales: 0,
-    no_cuenta: "",
+    carreraEstudiante: "",
+    generoPoetico: "",
 }
